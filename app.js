fde10a2d3ed92c57fad7119fbd8d246f4021ac4d
Glagny <nikoicardo@gmail.com>
2021-08-04 09:51:50 -0600

    Places UID and PID
    places Id, and places by user id.

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ const placesRoutes = require('./routes/places-routes');
 
 const app = express(); 
 
-app.use(placesRoutes);
+app.use('/api/places', placesRoutes); // => /api/places/...
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'}); // default error
+});  // error handler. Will execute if any previous middleware function yields an error
 
 app.listen(5000); 
 
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -5,10 +5,50 @@ const router = express.Router();
   but once configured, the router can be exported and imported 
   in app.js and registered as a single midleware in app.js*/
 
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famouse skyscrapers in the world.",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+];
 
-  router.get('/', (req, res, next) => {
-    console.log('GET Request in Places'); 
-    res.json({message: 'It works!'}); // sends back a response in JSON DATA
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // {pid: 'p1'}
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
   });
 
-  module.exports = router; 
+  if(!place) {
+    const error = new Error('Could not find for the provided place id.');
+    error.code = 404; 
+    throw error; 
+  }
+
+  res.json({ place }); // { place } => { place: place }
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid; // {pid: 'p1'}
+
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+
+  if(!place) {
+    const error = new Error('Could not find for the provided user id.');
+    error.code = 404; 
+    next(error); 
+  }
+
+  res.json({ place }); // { place } => { place: place }
+});
+
+module.exports = router;
