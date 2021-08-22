5609738a0c91a287a5c3d6271ff84085230b074a
Glagny <nikoicardo@gmail.com>
2021-08-22 15:06:43 -0600

    Added Token Authorization to Posting, Updating, and Deleting places.

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -3,16 +3,21 @@ const jwt = require('jsonwebtoken');
 const HttpError = require('../models/http-error');
 
 module.exports = (req, res, next) => {
+  if(req.method === 'OPTIONS') {
+    return next();
+  }
+
   try {
-    const token = req.headers.authorization.split(' ')[1]; // Authorization: 'Bearer TOKEN'
+    const token = req.headers.authorization.split(' ')[1]; 
+    console.log(req.headers.authorization.split(' ')[1]); // Authorization: 'Bearer TOKEN'
     if (!token) {
       throw new Error('Authentication failed!');
     }
-    const decoded_Token = jwt.verify(token, 'supersecret_dont_share');
-    req.userData = { userId: decoded_Token.userId }; // all requests after this middleware will be able to use that object.
+    const decodedToken = jwt.verify(token, 'supersecret_dont_share');
+    req.userData = { userId: decodedToken.userId }; // all requests after this middleware will be able to use that object.
     next();
   } catch (err) {
-    const error = new HttpError('Authentication failed!', 401);
+    const error = new HttpError('Authentication failed. ', 401);
     return next(error);
   }
 };
